5fb9d72dc664ee2f96adadaf53b4177f9e944de7
leochinacn <808431@qq.com>
2017-09-27 15:35:57 +0000

    finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+class SessionsController extends Controller
+{
+    public function create()
+    {
+      return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+      $this->validate($request,[
+        'email'=>'required|email|max:255',
+        'password'=>'required'
+      ]);
+
+      $loginData = [
+        'email'=>$request->email,
+        'password'=>$request->password
+      ];
+      if (Auth::attempt($loginData,$request->has('remember'))){
+        session()->flash('success','欢迎回来！');
+        return redirect()->route('users.show',Auth::user());
+      }else {
+        session()->flash('danger','不好意思，您的邮箱密码不匹配');
+        return redirect()->back();
+      }
+    }
+    public function destroy()
+    {
+      Auth::logout();
+      session()->flash('success', '您已成功退出！');
+     return redirect('login');
+    }
+}
